089bcdb04160a552a799564a2d0e4f7da4462fdd
gorgreat <egor.makarov@gmail.com>
2018-06-14 13:06:41 +0500

    добавил проверку - если время меньше дедлайна - то выводим нули

--- js/script.js
+++ js/script.js
@@ -35,18 +35,15 @@ window.addEventListener('DOMContentLoaded', function () {
   });
 
   //таймер
-  let deadLine = '2019-03-20';
+  let deadLine = '2018-03-20';
 
   /*parse - возвращает целое число, представляющее собой количество миллисекунд, истекших с полуночи 1 января 1970 года GMT+0 до даты, указанной в параметре dateVal*/
   function getTimeRemaining(endtime) {
-
     let t = Date.parse(endtime) - Date.parse(new Date()),
-      seconds = Math.floor((t / 1000) % 60), //округляем, берем милисекунды и переводим в секунды - получаем остаток от деления
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      /*        hours = Math.floor( (t / (1000 * 60 * 60)) );// - тут не верно*/
-      hours = Math.floor((t) % (24 * 60 * 60 * 1000) / (60 * 60 * 1000));
-
-
+        seconds = Math.floor((t / 1000) % 60), //округляем, берем милисекунды и переводим в секунды - получаем остаток от деления
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        /*        hours = Math.floor( (t / (1000 * 60 * 60)) );// - тут не верно*/
+        hours = Math.floor((t) % (24 * 60 * 60 * 1000) / (60 * 60 * 1000));        
     return {
       total: t,
       hours: hours,
@@ -56,21 +53,24 @@ window.addEventListener('DOMContentLoaded', function () {
   };
 
   function setClock(id, endtime) {
-
     let timer = document.getElementById(id),
-      hours = timer.querySelector('.hours'),
-      minutes = timer.querySelector('.minutes'),
-      seconds = timer.querySelector('.seconds');
-
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
     let timeInterval = setInterval(updateClock, 1000);
 
     //обновляем часы
     function updateClock() {
-
       let t = getTimeRemaining(endtime);
-      hours.innerHTML = addZero(t.hours);
-      minutes.innerHTML = addZero(t.minutes);
-      seconds.innerHTML = addZero(t.seconds);
+      if (t.total < 0) { //если мень дедлайна - то выводим нули
+        hours.innerHTML = '00';
+        minutes.innerHTML = '00';
+        seconds.innerHTML = '00';  
+      } else  {
+        hours.innerHTML = addZero(t.hours);
+        minutes.innerHTML = addZero(t.minutes);
+        seconds.innerHTML = addZero(t.seconds);  
+      }
 
       if (t.total <= 0) {
         clearInterval(timeInterval);
@@ -79,10 +79,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function addZero(num) {
       return ('0' + num).slice(-2); // обрезает 2 ненужных символа
-    }
+    }    
     
     updateClock();
-
 
   };
 
